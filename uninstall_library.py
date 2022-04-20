45cfc150b3b390eee0505295a205f07c9e424990
Joshua Sitompul <joshuasitompul2000@gmail.com>
2022-04-20 23:55:17 +0700

    revisi 3

---
+++ uninstall_library.py
@@ -0,0 +1,18 @@
+import os
+
+os.system("pip uninstall autopep8 -y")
+os.system("pip uninstall MouseInfo -y")
+os.system("pip uninstall numpy -y")
+os.system("pip uninstall opencv-python -y")
+os.system("pip uninstall Pillow -y")
+os.system("pip uninstall PyAutoGUI -y")
+os.system("pip uninstall pycodestyle -y")
+os.system("pip uninstall PyGetWindow -y")
+os.system("pip uninstall PyMsgBox -y")
+os.system("pip uninstall pyperclip -y")
+os.system("pip uninstall PyRect -y")
+os.system("pip uninstall PyScreeze -y")
+os.system("pip uninstall pytweening -y")
+os.system("pip uninstall setuptools -y")
+os.system("pip uninstall toml -y")
+print("\n Uninstall Complete...")
